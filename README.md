f670e39ab57538abba930295f42e4846b0579bea
reuben <user@volcan.lan>
2021-01-19 21:16:55 +0100

    Script is able to download and organize the MiddEval3 dataset

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ Initialize the virtual environment using
 source satinet-venv/bin/activate
 ```
 
+Things to install
+
+```console
+pip install progressist
+```
+
 # TODO List
 I need to fork to the s2p library from https://github.com/cmla/s2p.git - I need Johann's help on setting this up so that I can edit changes to the forked s2p repository.
 I need to fork to the mccnn library from https://github.com/Jackie-Chou/MC-CNN-python.git - this needs to be extensively updated including the conversion from python2 to python3. Again I need Johann's help to setup the fork.
--- data_preparation.py
+++ data_preparation.py
@@ -24,27 +24,70 @@
 import argparse
 import urllib.request
 import os, ssl
-
+import zipfile
+from progressist import ProgressBar
 # Specify the parser that will be used to get information from the command line
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                  description="Script to be used to download and organize the datasets.")
 parser.add_argument("--dataset", type=str, required=True, help="The dataset to be downloaded. The supported datasets are i) md for the Middlebury Stereo Vision or ii) IARPA Challenge")
 
-            
+def download_url(url, out_foldername):
+    # This is to create an unverified context to download https
+    ssl._create_default_https_context = ssl._create_unverified_context   
+    
+    # Derive the baseline name
+    filename = os.path.basename(url) 
+    bar = ProgressBar(template="Download |{animation}| {done:B}/{total:B}")
+   
+    # Download the file  
+    urllib.request.urlretrieve(url, filename=os.path.join(out_foldername,filename),reporthook=bar.on_urlretrieve) 
+     
+def unzip(filename,out_foldername):
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall(out_foldername)          
 def main(args):
     # Retrieve the arguments inputted from CLI	
     args = parser.parse_args()
     
-    # This is to create an unverified context to download https
-    ssl._create_default_https_context = ssl._create_unverified_context    
 
     if args.dataset == 'md':
         # Make sure there is a folder to contain the data
-        out_foldername = './data/MiddEval3/'
+        out_foldername = './data/'
         if not os.path.exists(out_foldername):
-            os.makedirs(out_foldername)		
-        urllib.request.urlretrieve("https://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-data-H.zip", filename=os.path.join(out_foldername,"MiddEval3-data-H.zip")) 
-	
+            os.makedirs(out_foldername)	
+        #########################################################################################################
+        # DOWNLOADING THE MIDDEVAL3 DATASET
+        #########################################################################################################
+        
+        # Download the MiddEval3-data-H.zip file
+        print('Downloading the MiddEval3-data-H.zip file')
+        download_url("https://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-data-H.zip",out_foldername)
+        
+        # Download the MiddEval3-GT0-H.zip file
+        print('Downloading the MiddEval3-GT0-H.zip file')
+        download_url('https://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-GT0-H.zip',out_foldername)
+        
+        # Download the MiddEval3-GT1-H.zip file
+        print('Downloading the MiddEval3-GT1-H.zip file')
+        download_url('https://vision.middlebury.edu/stereo/submit3/zip/MiddEval3-GT1-H.zip',out_foldername)
+
+        #########################################################################################################
+        # UNZIP THE THREE DOWNLOADED FILES
+        #########################################################################################################
+        print('Unzipping all the files in the dataset ...')
+        unzip(os.path.join(out_foldername,'MiddEval3-data-H.zip'), out_foldername)
+        unzip(os.path.join(out_foldername,'MiddEval3-GT0-H.zip'), out_foldername)
+        unzip(os.path.join(out_foldername,'MiddEval3-GT1-H.zip'), out_foldername)
+ 
+        #########################################################################################################
+        # CLEAN UP THE DATA
+        #########################################################################################################
+        print('Delete the zip files ...')
+        os.remove(os.path.join(out_foldername,'MiddEval3-data-H.zip'))
+        os.remove(os.path.join(out_foldername,'MiddEval3-GT0-H.zip'))
+        os.remove(os.path.join(out_foldername,'MiddEval3-GT1-H.zip'))
+        
+        
     return 0
 
 if __name__ == '__main__':
