c98932b4232e5b1868643da56fec871137d9bbf0
Reuben Farrugia <reuben.farrugia@um.edu.mt>
2021-01-20 15:52:31 +0100

    install dependencies for mccnn script

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 pip install --upgrade pip
 python3 -m pip install --upgrade setuptools
+
 pip install progressist==0.1.0
+pip install tensorflow==1.14.0
+pip install numpy==1.16.4
+pip install opencv-contrib-python==3.4.2.16
+pip install opencv-python==3.4.2.16
+pip install tqdm==4.50.0
