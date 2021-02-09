30aee13a0189f6c47507a50847a9738f09e61b61
Reuben Farrugia <reuben.farrugia@um.edu.mt>
2021-02-09 16:34:32 +0100

    updated script

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
+python3 -m venv --prompt SATINET venv
+source venv/bin/activate && \
+
 pip install --upgrade pip
 python3 -m pip install --upgrade setuptools
 
-pip install progressist==0.1.0
-pip install tensorflow==1.14.0
-pip install numpy==1.16.4
-pip install opencv-contrib-python==3.4.2.16
-pip install opencv-python==3.4.2.16
-pip install tqdm==4.50.0
+pip install -r requirements.txt
+
+#pip install progressist==0.1.0
+#pip install tensorflow==1.14.0
+#pip install numpy==1.16.4
+#pip install opencv-contrib-python==3.4.2.16
+#pip install opencv-python==3.4.2.16
+#pip install tqdm==4.50.0
