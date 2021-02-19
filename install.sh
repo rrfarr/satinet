b0f17ae71cb7ddb27e0e6c869e7ab8614c37ff47
reuben <reuben.farrugia@um.edu.mt>
2021-02-19 13:59:13 +0100

    Modified install.sh to reflect the exact order in which the packages were installed

--- install.sh
+++ install.sh
@@ -8,11 +8,42 @@ source venv/bin/activate
 
 # Upgrade the pip and setuptools
 # [these should not be necessary as you're installing requirements -JB]
-#pip install --upgrade pip
-#python3 -m pip install --upgrade setuptools
+pip install --upgrade pip
+
+
+pip install tensorflow-gpu==1.14.0
+pip install numpy==1.16.4
+pip install opencv-contrib-python==3.4.2.16
+pip install opencv-python==3.4.2.16
+pip install tqdm==4.50.0
+pip install matplotlib==3.3.2
+pip install imageio==2.9.0
+pip install scikit-image==0.17.2
+pip install memory_profiler==0.58.0
+pip install pandas==1.1.3
+pip install openpyxl==3.0.5
+pip install xlrd==1.2.0
+pip install rasterio==1.1.8
+pip install rpcm==1.4.7
+pip install bs4==0.0.1
+pip install ransac==1.0.2
+
+pip install s2p
+
+pip install silx==0.13.2
+pip install ad==1.3.2
+pip install ipywidgets==7.5.1
+pip install numba==0.52.0
+
+
+
+
+
+# NUMPY NEEDS TO BE INSTALLED BEFORE NUMBA
+#pip install numpy==1.16.4
 
 # Install the required packages
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # Installing the third party homography
 make -C homography
