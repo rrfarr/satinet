022b50222054945532d482e82842312e47483325
reuben <reuben.farrugia@um.edu.mt>
2021-02-26 12:52:34 +0100

    Cleaned some comments

--- mccnn-train.py
+++ mccnn-train.py
@@ -3,7 +3,7 @@
 #
 #  mccnn-train.py
 #  
-#  Developer Mang Chen, Reuben Farrugia
+#  Modified by Mang Chen, Reuben Farrugia
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
